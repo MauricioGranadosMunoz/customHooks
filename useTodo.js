35908b3c0b89401731d5f4c6d867b714055e1a56
StudentMauricioGranados <jgranados00042@ufide.ac.cr>
2022-07-25 21:10:30 -0600

    first commit

---
+++ useTodo.js
@@ -0,0 +1,45 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer";
+
+
+export const useTodo = () => {
+    const init = () => JSON.parse(localStorage.getItem('todos')) || []
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ))
+    }, [todos])
+    
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleRemoveTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+    
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+    return {
+        ...todos,
+        dispatch,
+        handleNewTodo,
+        handleRemoveTodo,
+        handleToggleTodo,
+        todoCount: todos.length,
+        pendingTodosCount: todos.filter( todo => !todo.done ).length,
+        todos,
+    }
+}
